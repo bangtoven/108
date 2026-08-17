ba87df9bcc8f2d30032c30638425cd80a3c951c1
Jünghø Bang <me@bangtoven.com>
2026-08-16 23:06:40 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'bow-counter-v5';
+const CACHE = 'bow-counter-v6';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
